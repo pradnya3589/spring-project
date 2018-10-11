3acb5592482ace1c07e0bcf22a84326c4879b9d3
Avadhut Kore <amkore01@gmail.com>
2018-10-11 07:55:29 +0530

    databse backup file updated

--- restapistarter_db.sql
+++ restapistarter_db.sql
@@ -55,6 +55,23 @@ CREATE TABLE `comments` (
 
 /*Data for the table `comments` */
 
+/*Table structure for table `employees` */
+
+DROP TABLE IF EXISTS `employees`;
+
+CREATE TABLE `employees` (
+  `company_id` varchar(255) NOT NULL,
+  `employee_id` varchar(255) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `phone_number` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`company_id`,`employee_id`),
+  UNIQUE KEY `UK_toga0mhsow9uddb96icomq65q` (`email`),
+  UNIQUE KEY `UK_g6512s2t9cous2oxa17he4irp` (`phone_number`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `employees` */
+
 /*Table structure for table `hibernate_sequence` */
 
 DROP TABLE IF EXISTS `hibernate_sequence`;
@@ -169,6 +186,19 @@ insert  into `oauth_refresh_token`(`token_id`,`token`,`authentication`) values
 ('429864336fdd9ae4b707ab54b2edfad4','¨Ì\0sr\0Lorg.springframework.security.oauth2.common.DefaultExpiringOAuth2RefreshToken/ﬂGcù–…∑\0L\0\nexpirationt\0Ljava/util/Date;xr\0Dorg.springframework.security.oauth2.common.DefaultOAuth2RefreshTokens·\ncT‘^\0L\0valuet\0Ljava/lang/String;xpt\0$a645749a-e554-4d69-8acc-09c99e1c2213sr\0java.util.DatehjÅKYt\0\0xpw\0\0f˙≈â˘x','¨Ì\0sr\0Aorg.springframework.security.oauth2.provider.OAuth2AuthenticationΩ@bR\0L\0\rstoredRequestt\0<Lorg/springframework/security/oauth2/provider/OAuth2Request;L\0userAuthenticationt\02Lorg/springframework/security/core/Authentication;xr\0Gorg.springframework.security.authentication.AbstractAuthenticationToken”™(~nGd\0Z\0\rauthenticatedL\0authoritiest\0Ljava/util/Collection;L\0detailst\0Ljava/lang/Object;xp\0sr\0&java.util.Collections$UnmodifiableList¸%1µÏé\0L\0listt\0Ljava/util/List;xr\0,java.util.Collections$UnmodifiableCollectionB\0ÄÀ^˜\0L\0cq\0~\0xpsr\0java.util.ArrayListxÅ“ô«aù\0I\0sizexp\0\0\0w\0\0\0sr\01com.tmkcomputers.apistarter.models.auth.Authorityd\Z;—¶¸\0L\0idt\0Ljava/lang/Long;L\0namet\0Ljava/lang/String;xpsr\0java.lang.Long;ã‰êÃè#ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî‡ã\0\0xp\0\0\0\0\0\0\0t\0COMPANY_READsq\0~\0\rsq\0~\0\0\0\0\0\0\0\0t\0DEPARTMENT_READxq\0~\0psr\0:org.springframework.security.oauth2.provider.OAuth2Request\0\0\0\0\0\0\0\0Z\0approvedL\0authoritiesq\0~\0L\0\nextensionst\0Ljava/util/Map;L\0redirectUriq\0~\0L\0refresht\0;Lorg/springframework/security/oauth2/provider/TokenRequest;L\0resourceIdst\0Ljava/util/Set;L\0\rresponseTypesq\0~\0xr\08org.springframework.security.oauth2.provider.BaseRequest6(z>£qiΩ\0L\0clientIdq\0~\0L\0requestParametersq\0~\0L\0scopeq\0~\0xpt\0(spring-security-oauth2-read-write-clientsr\0%java.util.Collections$UnmodifiableMapÒ•®˛tıB\0L\0mq\0~\0xpsr\0java.util.HashMap⁄¡√`—\0F\0\nloadFactorI\0	thresholdxp?@\0\0\0\0\0w\0\0\0\0\0\0t\0\ngrant_typet\0passwordt\0	client_idt\0(spring-security-oauth2-read-write-clientt\0usernamet\0readerxsr\0%java.util.Collections$UnmodifiableSetÄí—èõÄU\0\0xq\0~\0	sr\0java.util.LinkedHashSetÿl◊Zï›*\0\0xr\0java.util.HashSet∫DÖïñ∏∑4\0\0xpw\0\0\0?@\0\0\0\0\0t\0readt\0writexsq\0~\0,w\0\0\0?@\0\0\0\0\0sr\0Borg.springframework.security.core.authority.SimpleGrantedAuthority\0\0\0\0\0\0Ù\0L\0roleq\0~\0xpt\0USERxsq\0~\0!?@\0\0\0\0\0\0w\0\0\0\0\0\0\0xppsq\0~\0,w\0\0\0?@\0\0\0\0\0t\0resource-server-rest-apixsq\0~\0,w\0\0\0?@\0\0\0\0\0\0xsr\0Oorg.springframework.security.authentication.UsernamePasswordAuthenticationToken\0\0\0\0\0\0Ù\0L\0credentialsq\0~\0L\0	principalq\0~\0xq\0~\0sq\0~\0sq\0~\0\0\0\0w\0\0\0q\0~\0q\0~\0xq\0~\0;sr\0java.util.LinkedHashMap4¿N\\l¿˚\0Z\0accessOrderxq\0~\0!?@\0\0\0\0\0w\0\0\0\0\0\0q\0~\0#q\0~\0$q\0~\0%q\0~\0&q\0~\0\'q\0~\0(x\0psr\0,com.tmkcomputers.apistarter.models.auth.User%:l5W‘\0Z\0accountExpiredZ\0\raccountLockedZ\0credentialsExpiredZ\0enabledL\0authoritiesq\0~\0L\0idq\0~\0L\0passwordq\0~\0L\0usernameq\0~\0xp\0\0\0sr\0/org.hibernate.collection.internal.PersistentBagec˝? Ç\0\0L\0bagq\0~\0xr\0>org.hibernate.collection.internal.AbstractPersistentCollectionÈ”‹M©;•\0\nZ\0allowLoadOutsideTransactionI\0\ncachedSizeZ\0dirtyZ\0initializedZ\0\risTempSessionL\0keyt\0Ljava/io/Serializable;L\0ownerq\0~\0L\0roleq\0~\0L\0sessionFactoryUuidq\0~\0L\0storedSnapshotq\0~\0Bxp\0ˇˇˇˇ\0\0q\0~\0q\0~\0?t\08com.tmkcomputers.apistarter.models.auth.User.authoritiespsq\0~\0\0\0\0w\0\0\0q\0~\0q\0~\0xsq\0~\0\0\0\0w\0\0\0q\0~\0q\0~\0xq\0~\0t\0<$2a$08$dwYz8O.qtUXboGosJFsS4u19LHKW7aCQ0LXXuNlRfjjGKwj5NfKSet\0reader'),
 ('cbfd58453dc6336e60d2682e825f3f04','¨Ì\0sr\0Lorg.springframework.security.oauth2.common.DefaultExpiringOAuth2RefreshToken/ﬂGcù–…∑\0L\0\nexpirationt\0Ljava/util/Date;xr\0Dorg.springframework.security.oauth2.common.DefaultOAuth2RefreshTokens·\ncT‘^\0L\0valuet\0Ljava/lang/String;xpt\0$27536e23-652e-4e57-8aa4-dd4eeb79f82asr\0java.util.DatehjÅKYt\0\0xpw\0\0f˙ŸyNx','¨Ì\0sr\0Aorg.springframework.security.oauth2.provider.OAuth2AuthenticationΩ@bR\0L\0\rstoredRequestt\0<Lorg/springframework/security/oauth2/provider/OAuth2Request;L\0userAuthenticationt\02Lorg/springframework/security/core/Authentication;xr\0Gorg.springframework.security.authentication.AbstractAuthenticationToken”™(~nGd\0Z\0\rauthenticatedL\0authoritiest\0Ljava/util/Collection;L\0detailst\0Ljava/lang/Object;xp\0sr\0&java.util.Collections$UnmodifiableList¸%1µÏé\0L\0listt\0Ljava/util/List;xr\0,java.util.Collections$UnmodifiableCollectionB\0ÄÀ^˜\0L\0cq\0~\0xpsr\0java.util.ArrayListxÅ“ô«aù\0I\0sizexp\0\0\0w\0\0\0sr\01com.tmkcomputers.apistarter.models.auth.Authorityd\Z;—¶¸\0L\0idt\0Ljava/lang/Long;L\0namet\0Ljava/lang/String;xpsr\0java.lang.Long;ã‰êÃè#ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî‡ã\0\0xp\0\0\0\0\0\0\0t\0COMPANY_CREATEsq\0~\0\rsq\0~\0\0\0\0\0\0\0\0t\0COMPANY_READsq\0~\0\rsq\0~\0\0\0\0\0\0\0\0t\0COMPANY_UPDATEsq\0~\0\rsq\0~\0\0\0\0\0\0\0\0t\0COMPANY_DELETEsq\0~\0\rsq\0~\0\0\0\0\0\0\0\0t\0DEPARTMENT_CREATEsq\0~\0\rsq\0~\0\0\0\0\0\0\0\0t\0DEPARTMENT_READsq\0~\0\rsq\0~\0\0\0\0\0\0\0\0t\0DEPARTMENT_UPDATEsq\0~\0\rsq\0~\0\0\0\0\0\0\0\0t\0DEPARTMENT_DELETExq\0~\0psr\0:org.springframework.security.oauth2.provider.OAuth2Request\0\0\0\0\0\0\0\0Z\0approvedL\0authoritiesq\0~\0L\0\nextensionst\0Ljava/util/Map;L\0redirectUriq\0~\0L\0refresht\0;Lorg/springframework/security/oauth2/provider/TokenRequest;L\0resourceIdst\0Ljava/util/Set;L\0\rresponseTypesq\0~\0-xr\08org.springframework.security.oauth2.provider.BaseRequest6(z>£qiΩ\0L\0clientIdq\0~\0L\0requestParametersq\0~\0+L\0scopeq\0~\0-xpt\0(spring-security-oauth2-read-write-clientsr\0%java.util.Collections$UnmodifiableMapÒ•®˛tıB\0L\0mq\0~\0+xpsr\0java.util.HashMap⁄¡√`—\0F\0\nloadFactorI\0	thresholdxp?@\0\0\0\0\0w\0\0\0\0\0\0t\0\ngrant_typet\0passwordt\0	client_idt\0(spring-security-oauth2-read-write-clientt\0usernamet\0adminxsr\0%java.util.Collections$UnmodifiableSetÄí—èõÄU\0\0xq\0~\0	sr\0java.util.LinkedHashSetÿl◊Zï›*\0\0xr\0java.util.HashSet∫DÖïñ∏∑4\0\0xpw\0\0\0?@\0\0\0\0\0t\0readt\0writexsq\0~\0>w\0\0\0?@\0\0\0\0\0sr\0Borg.springframework.security.core.authority.SimpleGrantedAuthority\0\0\0\0\0\0Ù\0L\0roleq\0~\0xpt\0USERxsq\0~\03?@\0\0\0\0\0\0w\0\0\0\0\0\0\0xppsq\0~\0>w\0\0\0?@\0\0\0\0\0t\0resource-server-rest-apixsq\0~\0>w\0\0\0?@\0\0\0\0\0\0xsr\0Oorg.springframework.security.authentication.UsernamePasswordAuthenticationToken\0\0\0\0\0\0Ù\0L\0credentialsq\0~\0L\0	principalq\0~\0xq\0~\0sq\0~\0sq\0~\0\0\0\0w\0\0\0q\0~\0q\0~\0q\0~\0q\0~\0q\0~\0q\0~\0!q\0~\0$q\0~\0\'xq\0~\0Msr\0java.util.LinkedHashMap4¿N\\l¿˚\0Z\0accessOrderxq\0~\03?@\0\0\0\0\0w\0\0\0\0\0\0q\0~\05q\0~\06q\0~\07q\0~\08q\0~\09q\0~\0:x\0psr\0,com.tmkcomputers.apistarter.models.auth.User%:l5W‘\0Z\0accountExpiredZ\0\raccountLockedZ\0credentialsExpiredZ\0enabledL\0authoritiesq\0~\0L\0idq\0~\0L\0passwordq\0~\0L\0usernameq\0~\0xp\0\0\0sr\0/org.hibernate.collection.internal.PersistentBagec˝? Ç\0\0L\0bagq\0~\0xr\0>org.hibernate.collection.internal.AbstractPersistentCollectionÈ”‹M©;•\0\nZ\0allowLoadOutsideTransactionI\0\ncachedSizeZ\0dirtyZ\0initializedZ\0\risTempSessionL\0keyt\0Ljava/io/Serializable;L\0ownerq\0~\0L\0roleq\0~\0L\0sessionFactoryUuidq\0~\0L\0storedSnapshotq\0~\0Txp\0ˇˇˇˇ\0\0q\0~\0q\0~\0Qt\08com.tmkcomputers.apistarter.models.auth.User.authoritiespsq\0~\0\0\0\0w\0\0\0q\0~\0q\0~\0q\0~\0q\0~\0q\0~\0q\0~\0!q\0~\0$q\0~\0\'xsq\0~\0\0\0\0w\0\0\0q\0~\0q\0~\0q\0~\0q\0~\0q\0~\0q\0~\0!q\0~\0$q\0~\0\'xq\0~\0t\0<$2a$08$qvrzQZ7jJ7oy2p/msL4M0.l83Cd0jNsX6AJUitbgRXGzge4j035hat\0admin');
 
+/*Table structure for table `post_tags` */
+
+DROP TABLE IF EXISTS `post_tags`;
+
+CREATE TABLE `post_tags` (
+  `post_id` bigint(20) NOT NULL,
+  `tag_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`post_id`,`tag_id`),
+  KEY `FKm6cfovkyqvu5rlm6ahdx3eavj` (`tag_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `post_tags` */
+
 /*Table structure for table `posts` */
 
 DROP TABLE IF EXISTS `posts`;
@@ -185,6 +215,19 @@ CREATE TABLE `posts` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 /*Data for the table `posts` */
+
+/*Table structure for table `tags` */
+
+DROP TABLE IF EXISTS `tags`;
+
+CREATE TABLE `tags` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_121cwhh4hlnc550mh1tgm6a80` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `tags` */
 
 /*Table structure for table `testaddress` */
 
@@ -308,6 +351,45 @@ insert  into `user`(`id`,`account_expired`,`account_locked`,`credentials_expired
 (2,'\0','\0','\0','','$2a$08$dwYz8O.qtUXboGosJFsS4u19LHKW7aCQ0LXXuNlRfjjGKwj5NfKSe','reader'),
 (3,'\0','\0','\0','','$2a$08$kPjzxewXRGNRiIuL4FtQH.mhMn7ZAFBYKB3ROz.J24IX8vDAcThsG','modifier'),
 (4,'\0','\0','\0','','$2a$08$vVXqh6S8TqfHMs1SlNTu/.J25iUCrpGBpyGExA.9yI.IlDRadR6Ea','reader2');
+
+/*Table structure for table `user_phone_numbers` */
+
+DROP TABLE IF EXISTS `user_phone_numbers`;
+
+CREATE TABLE `user_phone_numbers` (
+  `user_id` bigint(20) NOT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  KEY `FKjnwj7m5dph0urds5ymb7ca3d3` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_phone_numbers` */
+
+/*Table structure for table `user_profiles` */
+
+DROP TABLE IF EXISTS `user_profiles`;
+
+CREATE TABLE `user_profiles` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `house_number` varchar(255) DEFAULT NULL,
+  `street` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `zip_code` varchar(255) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `middle_name` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(15) DEFAULT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_e5h89rk3ijvdmaiig4srogdc6` (`user_id`),
+  UNIQUE KEY `UK_dqltqkaw58m11jbov0udx8xqg` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_profiles` */
 
 /*Table structure for table `users_authorities` */
 
